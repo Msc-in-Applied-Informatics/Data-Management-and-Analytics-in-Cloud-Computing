c3cb97579821e851c87cef5fecdd73f217b02a5f
Karanik <nikolakis95@hotmail.com>
2026-05-11 18:25:13 +0300

    add B & C issues

--- redis-project/skeleton.java
+++ redis-project/skeleton.java
@@ -13,7 +13,7 @@ public class skeleton {
         
         while (true) {
             System.out.println("\n--- Μενού Redis Movies ---");
-            System.out.println("(I)nsert Artist | (Q)uery | (S)tatistics | e(X)it");
+            System.out.println("(I)nsert Movie | (Q)uery | (S)tatistics | e(X)it");
             System.out.print("Επιλογή: ");
             String choice = scanner.nextLine().toUpperCase();
             
@@ -33,7 +33,7 @@ public class skeleton {
                 String movieHashKey = "movie:" + key;
                 String watchListKey = "watchlist:" + key;
 
-                if(jedis.exists(key)){
+                if(jedis.exists(movieHashKey)){
                     jedis.sadd(watchListKey, username);
                     System.out.println("Η ταινία προστέθηκε στα watchlist");
                 }else{
@@ -54,16 +54,60 @@ public class skeleton {
                 String title = scanner.nextLine();
                 String key = title.toLowerCase();
                 String movieHashKey = "movie:" + key;
+                String watchListKey = "watchlist:" + key;
 
                 if(jedis.exists(movieHashKey)){
                     Map<String, String> movie = jedis.hgetAll(movieHashKey);
+                    jedis.sadd(watchListKey, username);
+
+                    long length = jedis.scard(watchListKey);
+                    System.out.println("Βρέθηκε: ");                    
                     System.out.println(movie);
+
+                    System.out.println("Συνολικό πλήθος χρηστών: " + length);
+                    
+                    // Trending
+                    jedis.zincrby("movie:trending", 1, movieHashKey);
+                    // History
+                    String historyKey = "user:" + username.toLowerCase() + ":history";
+                    if(jedis.llen(historyKey) == 5) 
+                        jedis.rpop(historyKey);
+                    
+                    jedis.lpush(historyKey, title);
+
+                    
                 }else{
                     System.out.println("Η ταινία με τίτλο " + title + " δεν βρέθηκε" );
                 }
                 
             } else if (choice.equals("S")) {
-                // TODO: Υλοποίηση στατιστικών
+                // Top Ταινίες
+                List<String> topMovies = jedis.zrevrange("movie:trending", 0, 2);
+                System.out.println("~~~~Top Ταινίες~~~~");
+                int i = 1;
+                for(String key : topMovies){
+                    Map<String, String> movie = jedis.hgetAll(key);
+                    System.out.println("Top-" + i +" :"  + movie);
+                    i++;
+                }
+                // User History
+                String historyKey = "user:" + username.toLowerCase() + ":history";
+                List<String> topSearch = jedis.lrange(historyKey, 0 , -1);
+                System.out.println("~~~~Top 5 Αναζητήσεις~~~~");
+                i = 1;
+                for(String searchKey : topSearch){
+                    System.out.println("-"+ i + ": " + searchKey);
+                    i++; 
+                } 
+                // Popularity Metrics
+                System.out.println("~~~~Popularity Metrics~~~~");
+                Set<String> watchlists = jedis.keys("watchlist:*");
+                for(String key: watchlists){
+                    String movie = key.replace("watchlist:","");
+                    long users = jedis.scard(key);
+                    System.out.println( movie + " -> " + users);
+                }
+                      
             } else if (choice.equals("X")) {
                 System.out.println("Έξοδος...");
                 break;
